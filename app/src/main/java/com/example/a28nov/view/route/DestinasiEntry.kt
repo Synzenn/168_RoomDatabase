d781ccebf6ab642a524c858b6be6ebd4ee9295fe
Synzenn <velriel42@gmail.com>
2025-11-28 13:35:41 +0700

    membuat val route, dan title yang dipanggil dari strings

--- app/src/main/java/com/example/a28nov/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a28nov/view/route/DestinasiEntry.kt
@@ -3,5 +3,6 @@ package com.example.a28nov.view.route
 import com.example.a28nov.R
 
 object DestinasiEntry: DestinasiNavigasi  {
-
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
