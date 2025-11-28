10ca8a5de28a13fe86be798eae88a442af3547be
Synzenn <velriel42@gmail.com>
2025-11-28 13:16:52 +0700

    membuat PenyediaViewModel.kt, lalu menambhakna library yang dibutuhkan

---
+++ app/src/main/java/com/example/a28nov/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.a28nov.viewmodel.provider
+
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a28nov.repositori.AplikasiSiswa
+import com.example.a28nov.viewmodel.EntryViewModel
+import com.example.a28nov.viewmodel.HomeViewModel
+
+object PenyediaViewModel{
