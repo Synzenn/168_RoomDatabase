f38ccf1b44449ba26b1a16f19097699acbd8292f
Synzenn <velriel42@gmail.com>
2025-11-28 13:20:20 +0700

    membuat fungsi siswaApp, dan hostNavigasi

--- app/src/main/java/com/example/a28nov/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a28nov/view/uicontroller/PetaNavigasi.kt
@@ -11,3 +11,27 @@ import com.example.a28nov.view.EntrySiswaScreen
 import com.example.a28nov.view.HomeScreen
 import com.example.a28nov.view.route.DestinasiEntry
 import com.example.a28nov.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+    }
+}
