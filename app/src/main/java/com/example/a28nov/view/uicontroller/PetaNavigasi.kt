e69a27f8ab721273099cf21e7121e5e5f23f71b9
Synzenn <velriel42@gmail.com>
2025-11-28 13:19:35 +0700

    membuat PetaNavigasi.kt, dan menambhakan library yang dibutuhkan

---
+++ app/src/main/java/com/example/a28nov/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,13 @@
+package com.example.a28nov.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.a28nov.view.EntrySiswaScreen
+import com.example.a28nov.view.HomeScreen
+import com.example.a28nov.view.route.DestinasiEntry
+import com.example.a28nov.view.route.DestinasiHome
