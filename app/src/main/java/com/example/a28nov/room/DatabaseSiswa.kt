74c89221c6d2f6d1de2efcd61869c7a3d761dc00
Synzenn <velriel42@gmail.com>
2025-11-28 13:22:58 +0700

    Membuat fungsi getDatabase, dan membuat roomdatabase yang bernama siswa_database

--- app/src/main/java/com/example/a28nov/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/a28nov/room/DatabaseSiswa.kt
@@ -6,3 +6,21 @@ import androidx.room.RoomDatabase
 import androidx.room.Room
 
 
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance : DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass= DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also {Instance=it}
+            })
+        }
+    }
+}
