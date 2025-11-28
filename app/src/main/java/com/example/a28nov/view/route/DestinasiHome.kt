bbca6c0669c43603316ffcc02f446655f2c9c195
Synzenn <velriel42@gmail.com>
2025-11-28 13:36:33 +0700

    membuat val route home, dan menambahkan title yang diapnggil dari strings

--- app/src/main/java/com/example/a28nov/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/a28nov/view/route/DestinasiHome.kt
@@ -3,5 +3,6 @@ package com.example.a28nov.view.route
 import com.example.a28nov.R
 
 object DestinasiHome: DestinasiNavigasi  {
-
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
