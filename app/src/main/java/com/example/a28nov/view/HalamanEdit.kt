30d032abc480ecbd87cd0e09bfa399a2936923de
Synzenn <velriel42@gmail.com>
2025-12-05 14:07:42 +0700

    membuat HalamanEdit.kt, dan menambahkan library yang dibutuhkan

---
+++ app/src/main/java/com/example/a28nov/view/HalamanEdit.kt
@@ -0,0 +1,15 @@
+package com.example.a28nov.view
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a28nov.view.route.DestinasiEditSiswa
+import com.example.a28nov.viewmodel.EditViewModel
+import com.example.a28nov.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
