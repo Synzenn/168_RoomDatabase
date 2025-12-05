a64224c20dbfc1d0ce370a58ff0a46d15666305c
Synzenn <velriel42@gmail.com>
2025-12-05 14:04:10 +0700

    membuat object DestinasiDetailSiswa

--- app/src/main/java/com/example/a28nov/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/a28nov/view/route/DestinasiDetailSiswa.kt
@@ -3,3 +3,9 @@ package com.example.a28nov.view.route
 
 import com.example.a28nov.R
 
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
